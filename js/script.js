cfd7b6a97b5c6d4d8986bf666c1a76aac2614176
MelinNathan <nathanmelin49@orange.fr>
2024-05-26 16:42:13 +0200

    i

--- js/script.js
+++ js/script.js
@@ -4,12 +4,12 @@ function myFunction() {
 
   element.classList.toggle("dark-mode");
   btn.classList.toggle("btn-warning");
-  if (btn.innerHTML === "Mode Nuit") {
-    btn.innerHTML = "Mode Jour";
-    localStorage.setItem("theme", "Mode Jour");
+  if (btn.innerHTML === "Mode Sombre") {
+    btn.innerHTML = "Mode Clair";
+    localStorage.setItem("theme", "Mode Clair");
   } else {
-    btn.innerHTML = "Mode Nuit";
-    localStorage.setItem("theme", "Mode Nuit");
+    btn.innerHTML = "Mode Sombre";
+    localStorage.setItem("theme", "Mode Sombre");
   }
 }
 
@@ -17,14 +17,14 @@ function initializeTheme() {
   var btn = document.getElementById('btn-dm');
   var theme = localStorage.getItem("theme");
 
-  if (theme === "Mode Jour") {
+  if (theme === "Mode Clair") {
     document.body.classList.add("dark-mode");
     btn.classList.add("btn-warning");
-    btn.innerHTML = "Mode Jour";
+    btn.innerHTML = "Mode Clair";
   } else {
     document.body.classList.remove("dark-mode");
     btn.classList.remove("btn-warning");
-    btn.innerHTML = "Mode Nuit";
+    btn.innerHTML = "Mode Sombre";
   }
 }
 
--- views/v_index.php
+++ views/v_index.php
@@ -8,7 +8,7 @@
 <p>Je m'appelle Nathan Melin</p>
 <p>J'ai <?=$age?> ans </p>
 
-
+<p>Je suis actuellement en BTS Services Informatiques aux Organisations option développemnt Web et Logiciel</p>
 
 
 <?php include 'views/v_footer.php' ?>
